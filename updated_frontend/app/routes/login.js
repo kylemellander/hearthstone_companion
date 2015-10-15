ce1ff79169fe543036689ac3420f62198d1756d6
Kyle Mellander <kylemellander@gmail.com>
2015-10-15 10:29:36 -0700

    Minor changes before testing new module

--- updated_frontend/app/routes/login.js
+++ updated_frontend/app/routes/login.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   actions: {
     signup() {
       var data = btoa(this.controller.identification + ":" + this.controller.password);
-      Ember.$.ajax({
+      var response = Ember.$.ajax({
         type: "POST",
         url: "http://localhost:3000/users",
         data: { signup_token: data },
