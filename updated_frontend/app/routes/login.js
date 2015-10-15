9311819b641067bdbe7696dce37727d09af294f0
Kyle Mellander <kylemellander@gmail.com>
2015-10-15 14:28:29 -0700

    Add error and success messages for login form

--- updated_frontend/app/routes/login.js
+++ updated_frontend/app/routes/login.js
@@ -15,15 +15,27 @@ export default Ember.Route.extend({
         var message = "<ul>";
         response.errors.forEach(function(error) {
           if (error.source.pointer === "/data/attributes/email") {
-            message += "<li>Email " + error.detail + ".</li>"
+            message += "<li>Email " + error.detail + ".</li>";
+          } else if (error.source.pointer === "/data/attributes/password") {
+            message += "<li>Password " + error.detail + ".</li>";
           }
         });
-        message += "</ul>"
-        $('#messages').empty().append(message);
+        message += "</ul>";
+        $('#messages').empty().append(message).removeClass("success").addClass("error");
       }).then(function(response) {
-        debugger;
+        if (response) {
+          $('#messages').empty().append("Your account has been created successfully!").removeClass("error").addClass("success");
+        }
       });
-
+    },
+    authenticate() {
+      var data = {identification: this.controller.identification, password: this.controller.password };
+      return this.get('session').authenticate('simple-auth-authenticator:devise', data).then(function(response) {
+        $('#messages').empty().append("You are successfully logged in.").removeClass("error").addClass("success");
+      }, function(reason) {
+        $('#messages').empty().append("Your username and/or password are incorrect.").removeClass("success").addClass("error");
+        // rejection
+      });
     }
   }
 });
