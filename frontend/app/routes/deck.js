f81ad0e75c3ba0b8742d03737860e7618bc48fd3
Kyle Mellander <kylemellander@gmail.com>
2015-10-30 16:24:31 -0700

    Add cardUsers to model for deck

--- frontend/app/routes/deck.js
+++ frontend/app/routes/deck.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.findRecord('deck', params.id);
+    return Ember.RSVP.hash({
+      deck: this.store.findRecord('deck', params.id),
+      cardUsers: this.store.findAll('cardUser')
+    });
   }
 });
