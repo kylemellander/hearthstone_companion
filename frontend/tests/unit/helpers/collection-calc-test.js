d15176946e36ac4d1ab3b37592b0defeb238f788
Kyle Mellander <kylemellander@gmail.com>
2015-10-30 12:22:51 -0700

    Refactor statistics into helpers

---
+++ frontend/tests/unit/helpers/collection-calc-test.js
@@ -0,0 +1,10 @@
+import { collectionCalc } from '../../../helpers/collection-calc';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | collection calc');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = collectionCalc(42);
+  assert.ok(result);
+});
