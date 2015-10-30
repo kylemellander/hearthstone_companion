c82f59d244962a60810656a178f1f5df156587b1
Kyle Mellander <kylemellander@gmail.com>
2015-10-30 12:40:26 -0700

    Refactor collection-calc further

--- frontend/app/helpers/collection-calc.js
+++ frontend/app/helpers/collection-calc.js
@@ -14,20 +14,16 @@ export function collectionCalc(params/*, hash*/) {
         sum += 1;
       } else if (params[1] === "your"){
         sum += object.get('count');
-      } else if (card.get('rarity') === "Legendary") {
-        sum += 1;
       } else {
         sum += 2;
       }
     }
     if (params[4] === "Promotion") {
       if (card.get(params[3]) === "Reward") {
-        if (params[2] === "unique") {
+        if (params[2] === "unique" || card.get('rarity') === "Legendary") {
           sum += 1;
         } else if (params[1] === "your"){
           sum += object.get('count');
-        } else if (card.get('rarity') === "Legendary") {
-          sum += 1;
         } else {
           sum += 2;
         }
