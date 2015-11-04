d182089bf227b40217956a28c372a2fbce70e968
Kyle Mellander <kylemellander@gmail.com>
2015-11-04 09:33:56 -0800

    Style deck list

--- frontend/app/components/deck-preview.js
+++ frontend/app/components/deck-preview.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'tr',
   cardsYouHave: Ember.computed.filter('cardUsers', function(cardUser) {
     var bool = false;
     this.get('cardDecks').forEach(function(cardDeck) {
