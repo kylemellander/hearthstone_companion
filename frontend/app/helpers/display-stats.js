2f9ef17a3eaeaebf783960cda1ad20a24e95485f
Kyle Mellander <student@epicodus.com>
2015-11-09 16:38:09 -0800

    Refactor stats page to style based on your collection percentage

---
+++ frontend/app/helpers/display-stats.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export function displayStats(params) {
+  function getColor(value){
+    //value from 0 to 1
+    var hue=((value)*120).toString(10);
+    return ["hsl(",hue,",75%,33%)"].join("");
+  };
+
+  var perc = Math.round(params[0]/params[1]*10000)/100;
+  return  '<span class="badge" style="background:' +
+          getColor(perc / 100) + '">' +
+          params[0] + ' / ' + params[1] +
+          ' ( ' + perc + '% )' + '</span>';
+}
+
+export default Ember.Helper.helper(displayStats);
---
+++ frontend/tests/unit/helpers/display-stats-test.js
@@ -0,0 +1,10 @@
+import { displayStats } from '../../../helpers/display-stats';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | display stats');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = displayStats(42);
+  assert.ok(result);
+});
